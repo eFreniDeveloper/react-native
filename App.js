f829f667362fa3fb7697b1260664c33281ac567c
Enrico Freni <enrico@freni.it>
2022-04-11 21:03:57 +0200

    added react-native-screen to improve performances

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import {enableScreens} from 'react-native-screens';
+
+enableScreens();
 
 export default function App() {
   return <MealsNavigator />;
